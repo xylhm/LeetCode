f29d39ae52cb1409c0cdf1c9c8599fcb45f92aa8
xylhm <gundamlelouch@gmail.com>
2017-09-06 16:04:42 +0800

    add a solution which got from someone's blog.

--- 461.Hamming Distance/461HammingDistance.cpp
+++ 461.Hamming Distance/461HammingDistance.cpp
@@ -2,7 +2,7 @@
 //
 
 #include "stdafx.h"
-int HammingDistance(int x, int y)
+int HammingDistance(int x, int y) //beat 26% in all c++ submit
 {
     int result = x ^ y;
     int count = 0;
@@ -18,10 +18,23 @@ int HammingDistance(int x, int y)
         ++count;
     return count;
 }
+//从网上看的
+// beat 3% = =，还没自己的好...因为每次都做了32次判断，O(n)
+int HammingDistance_bit(int x, int y) 
+{
+    int count = 0;
+    int result = x ^ y;
+    for(int i = 0; i < 32; ++i)
+    {
+        if ((result >> i) & 0x01)
+            ++count;
+    }
+    return count;
+}
 
 int main()
 {
-    printf("%d", HammingDistance(1, 12));
+    printf("%d", HammingDistance_bit(1, 12));
     getchar();
     return 0;
 }
